d965644a696bc08a67fcc3268aa59b6856435f47
Pirasanthan Jesugeevegan <pjes0001@Synchronoss.com>
2020-02-16 17:28:40 +0000

    Added Validation to the model by Type and requried data

--- api/models/products.js
+++ api/models/products.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
+    name: { type: String, required: true }, //Validation - Defining Type and Required
+    price: { type: Number, required: true }, //Validation - Defining Type and Required
 });
 
 module.exports = mongoose.model('Product', productSchema);
