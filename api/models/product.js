adbda01f236708ae1df20470018786ededeca4e2
Pirasanthan Jesugeevegan <pjes0001@Synchronoss.com>
2020-02-16 19:25:32 +0000

    Image Upload:  - Installed multer to allow image uploads  - added Image field to POST request & GET response  - Image Validation only accept jpg & png (fileFilter)  - Stablished the upload file (storage)

--- api/models/product.js
+++ api/models/product.js
@@ -4,6 +4,7 @@ const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true }, //Validation - Defining Type and Required
     price: { type: Number, required: true }, //Validation - Defining Type and Required
+    productImage: { type: String, }
 });
 
 module.exports = mongoose.model('Product', productSchema);
